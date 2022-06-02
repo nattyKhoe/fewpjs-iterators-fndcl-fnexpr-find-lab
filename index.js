27209aa4401343a8642fe1671e077136953fa43a
nattyKhoe <natasha.kusuma@outlook.com>
2022-06-02 22:20:21 +1000

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function isWin (KCC) {
+  return KCC.result == 'W';
+};
+function superbowlWin (record){
+  if ((record.find(isWin)) != null) {
+    return record.find(isWin).year;
+  } else {
+    return record.find(isWin);
+  }
+};
